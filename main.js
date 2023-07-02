5092e3eb6b66b40a5ac97a1e6e293502b089ebc1
DASURF <damilareyusuf.dy@gmail.com>
2023-07-02 10:36:11 +0100

    basic version

---
+++ main.js
@@ -0,0 +1,64 @@
+// Refereneces to DOM Elements
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const book = document.querySelector("#book");
+
+const paper1 = document.querySelector("#p1");
+const paper2 = document.querySelector("#p2");
+const paper3 = document.querySelector("#p3");
+
+// Event Listeners
+
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener("click", goNexPage);
+
+let currentLocation = 1;
+let numOfPapers = 3;
+let nextLocation = numOfPapers + 1;
+
+
+
+function goNexPage() {
+    if (currentLocation < nextLocation) {
+        switch(currentLocation) {
+            case 1:
+                paper1.classList.add("flipped");   
+                paper1.style.zIndex = 1;
+                break;
+            case 2:
+                paper2.classList.add("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 3:
+                paper3.classList.add("flipped");
+                paper3.style.zIndex = 3;
+                break;
+            default:
+                throw new Error("unknown state");
+        }
+        currentLocation++;
+    }
+
+}
+
+function goPrevPage() {
+    if (currentLocation > 1) {
+        switch(currentLocation) {
+            case 2:
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex = 3;
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 4:
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex = 1;
+                break;
+            default:
+                throw new Error("Unknown state");
+        }
+        currentLocation--;
+    }
+}
